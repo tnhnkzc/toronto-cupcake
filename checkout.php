15a7fccd337fe45fcc906f2ea3f728c8781bd3a2
Tunahan Kuzucu <tunahankuzucuu@gmail.com>
2023-01-12 15:01:37 +0100

    feat: add custom page template for checkout page, start styling

---
+++ checkout.php
@@ -0,0 +1,31 @@
+<?php
+
+/* Template Name: checkout
+
+
+/* 
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Toronto_Cupcake
+ */
+
+get_header();
+?>
+
+<main id="primary" class="site-checkout">
+
+  <?php
+  while (have_posts()) :
+    the_post();
+
+    get_template_part('template-parts/content', 'page');
+
+    // If comments are open or we have at least one comment, load up the comment template.
+    if (comments_open() || get_comments_number()) :
+      comments_template();
+    endif;
+
+  endwhile; // End of the loop.
+  ?>
+
+</main><!-- #main -->
