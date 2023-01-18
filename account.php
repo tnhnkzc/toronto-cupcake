476ae30fa17b542e004d6182c1b25faf1b83c7b3
Tunahan Kuzucu <tunahankuzucuu@gmail.com>
2023-01-18 15:51:03 +0100

    feat(account page): start styling of the account page

--- account.php
+++ account.php
@@ -1,17 +1,19 @@
 <?php
 /* Template Name: account */
-
-
-
 get_header();
 ?>
 <main id="primary" class="site-main">
-
   <?php
   while (have_posts()) :
     the_post();
+  ?>
+    <div class="content">
+      <?php get_template_part('template-parts/content', 'page'); ?>
+    </div>
 
-    get_template_part('template-parts/content', 'page');
-
-
+  <?php
   endwhile;
+  get_footer();
+  ?>
+
+</main>
