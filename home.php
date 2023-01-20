d5dbd18e5e7557126e15a00e2c782d34ccb0b47f
Tunahan Kuzucu <tunahankuzucuu@gmail.com>
2023-01-20 16:05:47 +0100

    feat: add home page template, try fetch custom posts not working yet

---
+++ home.php
@@ -0,0 +1,105 @@
+<?php
+/* Template Name: Home */
+/* @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Toronto_Cupcake
+ */
+
+get_header();
+
+
+$args = array(
+  'post_type' => 'post',
+  'post_status' => 'publish',
+);
+$informations = new WP_Query($args);
+?>
+
+<main id="primary" class="site-main">
+  <div class="banner">
+    <h4>Shipping is now free for a limited time !</h4>
+  </div>
+  <section class="sliderSection">
+    <?php echo do_shortcode('[metaslider id="12"]'); ?>
+    <div class="btnDiv">
+      <a class="bouton" href="<?= site_url(); ?>/shop">Shop Now</a>
+    </div>
+  </section>
+  <section class="cupcakeSection" id="cupcake">
+    <div class="cupcakesTitleDiv">
+      <h3 class="cupcakesTitle">Best Selling Cupcakes</h3>
+    </div>
+    <div class="cupcakes">
+      <?php echo do_shortcode('[products limit="3" columns="3"]'); ?>
+    </div>
+  </section>
+  <section class="eventSection" id="events">
+    <div class="eventTitle">
+      <?php if ($informations->have_posts()) :
+        while ($informations->have_posts()) :
+          $data->the_post();
+      ?>
+          <h3><?php echo get_the_title(32) ?></h3>
+      <?php
+        endwhile;
+      endif;
+      wp_reset_postdata();  ?>
+    </div>
+    <div class="events">
+      <div class="event-1">
+        <h4>Corporate and Marketing Events</h4>
+        <p class="eventText">
+          From Product/Brand Launch to Customer Appreciation, Toronto Cupcake is able to provide you custom decorating to match that winning campaign. Our graphics are crystal clear and are edible!
+        </p>
+      </div>
+      <div class="event-2">
+        <h4>Occasions</h4>
+        <p class="eventText">
+          A celebration of life, love, and all of life's memorable events. We have over 1500 different combinations with our standard offerings but if you want something special that is also possible !
+        </p>
+      </div>
+    </div>
+    <div class="clients">
+      <h4>Some of Our Clients</h4>
+      <div class="images">
+        <img src="<?= bloginfo('template_directory'); ?>/assets/images/versace.png" alt="">
+        <img src="<?= bloginfo('template_directory'); ?>/assets/images/android.png" alt="">
+        <img src="<?= bloginfo('template_directory'); ?>/assets/images/bmw.png" alt="">
+        <img src="<?= bloginfo('template_directory'); ?>/assets/images/ibm.png" alt="">
+        <img src="<?= bloginfo('template_directory'); ?>/assets/images/linkedin.png" alt="">
+      </div>
+  </section>
+  <section class="aboutSection" id="about">
+    <div class="aboutTitle">
+      <h3>
+        <?php the_title() ?>
+      </h3>
+    </div>
+    <div class="entryText">
+      <h5>We are driven by loving what we do and what we make everyday. We get to use the finest ingredients to make what we believe are the tastiest treats around. </h5>
+      <p>We are driven by loving what we do and what we make everyday. We get to use the finest ingredients to make what we believe are the tastiest treats around. </p>
+    </div>
+    <div class="michelle">
+      <img src="<?= bloginfo('template_directory'); ?>/assets/images/MichellePhoto.png" alt="">
+      <h5>About Michelle
+        Toronto Cupcake was created by Michelle Harrison so she could pursue her love of baking. A lifelong baker, inspired by her mother, Michelle opened Toronto Cupcake in August 2010 as one of Canada's first gourmet cupcakeries.</p>
+    </div>
+  </section>
+  <section class="contactSection" id="contact">
+    <div class="contactTitle">
+      <h3>Contact Us</h3>
+    </div>
+    <div class="contact">
+      <div class="imageDiv">
+        <img class="image" src="<?= bloginfo('template_directory'); ?>/assets/images/contactLetter.png" alt="">
+      </div>
+      <div class="contactForm"><?php echo do_shortcode('[contact-form-7 id="11" title="Contact form 1"]'); ?>
+      </div>
+    </div>
+
+  </section>
+</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
